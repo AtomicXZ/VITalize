aedc1e31554c358f4041cace31bfe6115e0c6686
AtomicXZ <achubadyal4@gmail.com>
2023-08-07 10:05:45 +0530

    refactor time utils

--- lib/1_app/core/utils/time_utils.dart
+++ lib/1_app/core/utils/time_utils.dart
@@ -1,32 +1,17 @@
 String get todayAsWord {
   int day = DateTime.now().weekday;
 
-  switch (day) {
-    case 2:
-      return 'Tuesday';
-    case 3:
-      return 'Wednesday';
-    case 4:
-      return 'Thursday';
-    case 5:
-      return 'Friday';
-    case 6:
-      return 'Saturday';
-    default:
-      return 'Holiday';
-  }
+  final dayNames = {
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+  };
+  return dayNames[day] ?? 'Holiday';
 }
 
-String getCurrentTime() {
+String get getCurrentTime {
   final DateTime now = DateTime.now();
-  final String twoDigitHour = _twoDigits(now.hour);
-  final String twoDigitMinute = _twoDigits(now.minute);
-  return '$twoDigitHour:$twoDigitMinute';
-}
-
-String _twoDigits(int n) {
-  if (n >= 10) {
-    return '$n';
-  }
-  return '0$n';
+  return '${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}';
 }
