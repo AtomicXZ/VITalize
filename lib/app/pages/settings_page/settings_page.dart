5f64066bd5f2f8598c97bc1d63f4baed3ddb2f1a
AtomicXZ <achubadyal4@gmail.com>
2023-08-28 18:33:10 +0530

    settings_page: show dialog to choose color

--- lib/app/pages/settings_page/settings_page.dart
+++ lib/app/pages/settings_page/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
+import 'package:go_router/go_router.dart';
 import 'package:vitalize/app/core/theme/vitalise_theme.dart';
 import 'package:vitalize/app/core/utils/hive_user_box_utils.dart';
 import 'package:vitalize/app/core/widgets/animated_column.dart';
@@ -50,25 +51,46 @@ class _SettingsPageState extends State<SettingsPage> {
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: 16),
           child: ListTile(
-              enabled: !_dynamicTheme,
-              leading: const Icon(FluentIcons.color_24_regular),
-              title: const Text('Custom Theme Color'),
-              subtitle: _dynamicTheme
-                  ? const Text('Disabled')
-                  : Padding(
-                      padding: const EdgeInsets.only(top: 16.0),
-                      child: MaterialColorPicker(
-                        shrinkWrap: true,
-                        selectedColor: Color(
-                            int.parse(getString(themeColor) ?? primaryColor)),
-                        onColorChange: (color) {
-                          setKeyValue(themeColor,
-                              color.toString().split('(')[1].substring(0, 10));
-                        },
-                      ),
-                    )),
+            enabled: !_dynamicTheme,
+            leading: const Icon(FluentIcons.color_24_regular),
+            title: const Text('Custom Theme Color'),
+            trailing: ElevatedButton(
+              onPressed: _dynamicTheme ? null : _openColorPickerDialog,
+              onLongPress: null,
+              child: !_dynamicTheme
+                  ? const Text('Pick color')
+                  : const Text('Disabled'),
+            ),
+          ),
         ),
       ),
     ]);
+  }
+
+  void _openColorPickerDialog() {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          contentPadding: const EdgeInsets.all(18.0),
+          title: const Text('Color Picker'),
+          content: MaterialColorPicker(
+            colors: accentColors,
+            selectedColor:
+                Color(int.parse(getString(themeColor) ?? primaryColor)),
+            onColorChange: (color) {
+              setKeyValue(
+                  themeColor, color.toString().split('(')[1].substring(0, 10));
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: context.pop,
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
