ef90854d1119928b595f7a48012cd7a32df451b2
AtomicXZ <achubadyal4@gmail.com>
2023-09-03 19:58:41 +0530

    marks_page: redo semID selection dropdown

--- lib/app/pages/marks_page/widgets/marks_page_content.dart
+++ lib/app/pages/marks_page/widgets/marks_page_content.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,46 +9,64 @@ import 'package:vitalize/app/pages/marks_page/cubit/semester_id_menu_cubit.dart'
 import 'package:vitalize/app/pages/marks_page/widgets/marks_card.dart';
 
 class MarksPageContent extends StatelessWidget {
-  final SemesterIDsMenuLoaded state;
+  final SemesterIDsMenuLoaded semIDstate;
 
-  const MarksPageContent(this.state, {super.key});
+  const MarksPageContent(this.semIDstate, {super.key});
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
     return Column(
       children: [
-        ConstrainedBox(
-          constraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width * 0.95,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 4),
-            child: DropdownMenu(
-              onSelected: (value) =>
-                  BlocProvider.of<MarksPageCubit>(context).getMarks(value),
-              menuHeight: MediaQuery.of(context).size.height * 0.95,
-              menuStyle: const MenuStyle().copyWith(
-                shape: MaterialStatePropertyAll(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
+        BlocBuilder<MarksPageCubit, MarksPageState>(
+          builder: (context, state) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 4),
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton2(
+                  isExpanded: true,
+                  value: BlocProvider.of<MarksPageCubit>(context).semID == ''
+                      ? null
+                      : BlocProvider.of<MarksPageCubit>(context).semID,
+                  hint: const Text('Select Semester'),
+                  buttonStyleData: ButtonStyleData(
+                    height: 75,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    padding: const EdgeInsets.only(left: 14, right: 14),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      border: Border.all(
+                        color: colorScheme.secondary,
+                      ),
+                      color: colorScheme.surface,
+                    ),
+                    elevation: 2,
                   ),
+                  iconStyleData: IconStyleData(
+                    icon: const Icon(
+                      Icons.arrow_forward_ios_outlined,
+                    ),
+                    iconSize: 14,
+                    iconEnabledColor: colorScheme.primary,
+                  ),
+                  dropdownStyleData: DropdownStyleData(
+                    maxHeight: 400,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: colorScheme.secondaryContainer,
+                    ),
+                    offset: const Offset(0, -10),
+                  ),
+                  onChanged: (value) => BlocProvider.of<MarksPageCubit>(context)
+                      .getMarks(value.toString()),
+                  items: semIDstate.semIDs.entries
+                      .map((e) =>
+                          DropdownMenuItem(value: e.key, child: Text(e.value)))
+                      .toList(),
                 ),
               ),
-              label: const Text(
-                'Select Semester',
-                style: TextStyle(fontSize: 16),
-              ),
-              inputDecorationTheme: InputDecorationTheme(
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-              ),
-              dropdownMenuEntries: state.semIDs.entries
-                  .map((e) => DropdownMenuEntry(value: e.key, label: e.value))
-                  .toList(),
-            ),
-          ),
+            );
+          },
         ),
         const SizedBox(height: 8),
         BlocBuilder<MarksPageCubit, MarksPageState>(
